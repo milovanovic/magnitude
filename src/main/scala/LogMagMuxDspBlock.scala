48ec8215a88c8205d98a975cc93804c87d9a9456
milovanovic <barun@eecs.berkeley.edu>
2021-07-06 16:42:38 +0200

    Switch to master/slave node and make some changes

--- src/main/scala/LogMagMuxDspBlock.scala
+++ src/main/scala/LogMagMuxDspBlock.scala
@@ -3,10 +3,8 @@
 package magnitude
 
 import chisel3._
-import chisel3.util._
 import chisel3.experimental._
 
-import dsptools._
 import dsptools.numbers._
 
 import dspblocks._
@@ -17,24 +15,71 @@ import freechips.rocketchip.diplomacy._
 import freechips.rocketchip.regmapper._
 import freechips.rocketchip.tilelink._
 
+// make standalone block for LogMagMux
+trait AXI4LogMagMuxStandaloneBlock extends AXI4LogMagMuxBlock[FixedPoint] {
+  def standaloneParams = AXI4BundleParameters(addrBits = 32, dataBits = 32, idBits = 1)
+  val ioMem = mem.map { m => {
+    val ioMemNode = BundleBridgeSource(() => AXI4Bundle(standaloneParams))
+
+    m :=
+      BundleBridgeToAXI4(AXI4MasterPortParameters(Seq(AXI4MasterParameters("bundleBridgeToAXI4")))) :=
+      ioMemNode
+
+    val ioMem = InModuleBody { ioMemNode.makeIO() }
+    ioMem
+  }}
+
+  val ioInNode = BundleBridgeSource(() => new AXI4StreamBundle(AXI4StreamBundleParameters(n = 2)))
+  val ioOutNode = BundleBridgeSink[AXI4StreamBundle]()
+
+  ioOutNode :=
+    AXI4StreamToBundleBridge(AXI4StreamSlaveParameters()) :=
+    streamNode :=
+    BundleBridgeToAXI4Stream(AXI4StreamMasterParameters(n = 2)) :=
+    ioInNode
+
+  val in = InModuleBody { ioInNode.makeIO() }
+  val out = InModuleBody { ioOutNode.makeIO() }
+}
+
 abstract class LogMagMuxBlock [T <: Data : Real: BinaryRepresentation, D, U, E, O, B <: Data] (params: MAGParams[T], beatBytes: Int) extends LazyModule()(Parameters.empty) with DspBlock[D, U, E, O, B] with HasCSR {
 
-  val streamNode = AXI4StreamIdentityNode()
+  // this block requires that sel signal is there, no HasCSR when JPL only or Mag Sqr only or LogMag only is set
+  require(params.magType == MagJPLandSqrMag || params.magType == MagJPLandLogMag || params.magType == LogMagMux)
+
+  val dataWidthOut = params.protoOut.getWidth
+
+  val masterParams = AXI4StreamMasterParameters(
+    name = "AXI4 Stream Accumulator",
+    n = (dataWidthOut + 8 - 1) / 8,
+    numMasters = 1
+  )
+  val slaveParams = AXI4StreamSlaveParameters()
+
+  val slaveNode  = AXI4StreamSlaveNode(slaveParams)
+  val masterNode = AXI4StreamMasterNode(masterParams)
+
+  val streamNode = NodeHandle(slaveNode, masterNode)
+
 
   lazy val module = new LazyModuleImp(this) {
-    val (in, _)  = streamNode.in(0)
-    val (out, _) = streamNode.out(0)
+    val (in, edgeIn)  = slaveNode.in.head
+    val (out, edgeOut) = masterNode.out.head
 
     //  Log magnitude mux module
     val logMagMux = Module(new LogMagMuxGenerator(params))
-    
-    val selReg         = RegInit(0.U(2.W))
+
+    // if it is only one type then this register should not be considered at all
+    val selRegDataWidth = if (params.magType == LogMagMux) 2 else 1
+
+    val selReg = RegInit(0.U(selRegDataWidth.W))
     val fields = Seq(
-      RegField(2, selReg,
+      RegField(selRegDataWidth, selReg,
         RegFieldDesc(name = "sel", desc = "selection signal for the log magnitude multiplexer")),
       // left for future addition
     )
     logMagMux.io.sel := selReg
+
     // Define abstract register map so it can be AXI4, Tilelink, APB, AHB
     regmap(fields.zipWithIndex.map({ case (f, i) => i * beatBytes -> Seq(f)}): _*)
     
@@ -79,7 +124,7 @@ object LogMagMuxDspBlock extends App
   // here just define parameters
   val params: MAGParams[FixedPoint] =  MAGParams(
     protoIn  = FixedPoint(16.W, 8.BP),
-    protoOut = FixedPoint(16.W, 8.BP),
+    protoOut = FixedPoint(20.W, 8.BP),
     protoLog = Some(FixedPoint(16.W, 8.BP)),
     magType  = MagJPLandLogMag,
     log2LookUpWidth = 8,
@@ -91,8 +136,6 @@ object LogMagMuxDspBlock extends App
   val baseAddress = 0x500
   implicit val p: Parameters = Parameters.empty
   
-  val mag = LazyModule(new AXI4LogMagMuxBlock(params, AddressSet(baseAddress + 0x100, 0xFF), _beatBytes = 4) with dspblocks.AXI4StandaloneBlock {
-    override def standaloneParams = AXI4BundleParameters(addrBits = 32, dataBits = 32, idBits = 1)
-  })
+  val mag = LazyModule(new AXI4LogMagMuxBlock(params, AddressSet(baseAddress + 0x100, 0xFF), _beatBytes = 4) with AXI4LogMagMuxStandaloneBlock)
   chisel3.Driver.execute(args, ()=> mag.module)
 }
